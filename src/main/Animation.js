48f50b30af24632a4b1e094ba7f6c83629d2c8ca
Mark <Markklanser@gmail.com>
2018-06-17 13:57:43 +0200

    Complete rework of workpanel, single panel

--- src/main/Animation.js
+++ src/main/Animation.js
@@ -1,21 +1,10 @@
 $(document).ready(function() {
     var lastClicked = 'home';
-
     var cvClicked = null;
 
-    function animateWorkPanels(percentage) {
-        $('#workA').animate({'margin-top': percentage + '%'}, 1000);
-        $('#workATitle').delay(700).fadeToggle(1000);
-        $('#workASubTitle').delay(1000).fadeToggle(1000);
-        $('#workB').delay(300).animate({'margin-top': percentage + '%'}, 1000);
-        $('#workBTitle').delay(1300).fadeToggle(1000);
-        $('#workBSubTitle').delay(1600).fadeToggle(1000);
-        $('#workC').delay(600).animate({'margin-top': percentage + '%'}, 1000);
-        $('#workCTitle').delay(1900).fadeToggle(1000);
-        $('#workCSubTitle').delay(2200).fadeToggle(1000);
-        $('#workD').delay(900).animate({'margin-top': percentage + '%'}, 1000);
-        $('#workDTitle').delay(2400).fadeToggle(1000);
-        $('#workDSubTitle').delay(2600).fadeToggle(1000);
+    function toggleWorkTitles () {
+        $('.workTitle').delay(700).fadeToggle(1000);
+        $('.workSubTitle').delay(1000).fadeToggle(1000);
     }
 
     $("#homeLink").on("click", function (e) {
@@ -27,8 +16,7 @@ $(document).ready(function() {
             }
             $(".navButton").css("pointer-events", "none");
             lastClicked = 'home';
-            $('#about').slideToggle(1500);
-            $('#filter').delay(1500).animate({right: '100%'}, "slow");
+            $('#about').animate({'margin-top': -100 + '%'}, 1000);
             setTimeout(function () {
                 $(".navButton").delay(8000).css("pointer-events", "auto");
             }, 1500)
@@ -36,8 +24,8 @@ $(document).ready(function() {
         if (lastClicked == 'work') {
             $(".navButton").css("pointer-events", "none");
             lastClicked = 'home';
-            animateWorkPanels(100);
-            $('#filter').delay(1000).animate({right: '100%'}, "slow");
+            $('#workPanel').animate({'margin-top': 100 + '%'}, 1000);
+            toggleWorkTitles();
             setTimeout(function () {
                 $(".navButton").delay(8000).css("pointer-events", "auto");
             }, 1800)
@@ -49,20 +37,20 @@ $(document).ready(function() {
         if (lastClicked == 'home') {
             $(".navButton").css("pointer-events", "none");
             lastClicked = 'about';
-            $('#filter').animate({right: '0%'}, "slow");
-            $('#about').delay(500).slideToggle(1000);
+            $('#about').animate({'margin-top': 0 + '%'}, 1000);;
             setTimeout(function () {
-                $(".navButton").delay(8000).css("pointer-events", "auto");
+                $(".navButton").css("pointer-events", "auto");
             }, 1500)
         }
         if (lastClicked == 'work') {
             $(".navButton").css("pointer-events", "none");
             lastClicked = 'about';
-            animateWorkPanels(100);
-            $('#about').delay(1000).slideToggle(1000);
+            $('#about').animate({'margin-top': 0 + '%'}, 1000);
+            $('#workPanel').animate({'margin-top': 100 + '%'}, 1000);
+            toggleWorkTitles();
             setTimeout(function () {
-                $(".navButton").delay(8000).css("pointer-events", "auto");
-            }, 2000)
+                $(".navButton").css("pointer-events", "auto");
+            }, 1500)
         }
     });
 
@@ -71,11 +59,11 @@ $(document).ready(function() {
         if (lastClicked == 'home') {
             $(".navButton").css("pointer-events", "none");
             lastClicked = 'work';
-            $('#filter').animate({right: '0%'}, "slow");
-            animateWorkPanels(0);
+            $('#workPanel').animate({'margin-top': 0 + '%'}, 1000);
+            toggleWorkTitles();
             setTimeout(function () {
-                $(".navButton").delay(8000).css("pointer-events", "auto");
-            }, 4000)
+                $(".navButton").delay(2000).css("pointer-events", "auto");
+            }, 1500)
         }
         if (lastClicked == 'about') {
             if (cvClicked == true) {
@@ -84,11 +72,12 @@ $(document).ready(function() {
             }
             $(".navButton").css("pointer-events", "none");
             lastClicked = 'work';
-            $('#about').slideToggle(1000);
-            animateWorkPanels(0);
+            $('#about').animate({'margin-top': -100 + '%'}, 1000);
+            $('#workPanel').animate({'margin-top': 0 + '%'}, 1000);
+            toggleWorkTitles();
             setTimeout(function () {
                 $(".navButton").delay(8000).css("pointer-events", "auto");
-            }, 4000)
+            }, 1500)
         }
     });
 
